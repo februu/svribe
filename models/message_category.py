6503fecf6d2f9d942990cbf10b19cac4335316f5
febru <81876955+februu@users.noreply.github.com>
2026-07-10 14:44:26 +0200

    Add MessageCategory validation

--- models/message_category.py
+++ models/message_category.py
@@ -10,3 +10,19 @@ class MessageCategory:
     name: str
     description: str
     channel: discord.TextChannel
+
+    def __post_init__(self):
+        if not self.name == self.name.strip().lower():
+            raise ValueError(
+                "Category name must be lowercase and cannot have leading or trailing whitespace."
+            )
+        if not self.name == self.channel.name.strip().lower():
+            raise ValueError("Category name must match the associated channel name.")
+        if self.name.lower() == "<none>":
+            raise ValueError(
+                "The name '<none>' is reserved and cannot be used for a category."
+            )
+        if not self.description == self.description.strip():
+            raise ValueError(
+                "Category description cannot have leading or trailing whitespace."
+            )
